703462bc3f2e6973e687a7b51f4a42a056ac0001
Gideon Jacobus Joubert <spastika@Gideons-MacBook-Pro.local>
2019-10-19 10:14:12 +0200

    Translate to Afrikaans for a bit of cusomization

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Verander <code>src/App.js</code> and stoor - herlaai!
         </p>
         <a
           className="App-link"
